ffe3945450c909a75c0ba42939c7344522d22bd8
Yun-Yi Lin <lordlosslyy@gmail.com>
2021-08-30 00:45:35 +0800

    change iframe back with title

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -10,7 +10,7 @@ const VideoDetail = ({video}) => {
     return (
         <div>
             <div className="ui embed">
-                <iframe src={videoSrc} />
+                <iframe title={video.snippet.title} src={videoSrc} />
             </div>
             <div className="ui segment">
                 <h4 className="ui header">{video.snippet.title}</h4>
